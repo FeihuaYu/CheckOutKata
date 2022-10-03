43cbbf4cc2a064519bed7777e64202902c6d0b32
脸宝 <zbb@s-MacBook-Pro.local>
2022-10-03 18:40:05 +0100

    clean code

--- src/main/TotalPrice.java
+++ src/main/TotalPrice.java
@@ -7,18 +7,16 @@ import java.util.Map;
 
 
 public class TotalPrice {
-    ShoppingCart itemsList;
-    PromotionItems promotionItems;
+    private PromotionItems promotionItems;
+    private Map<Item, Integer> itemsMap;
 
     public TotalPrice(ShoppingCart itemsList, PromotionItems promotionItems) {
-        this.itemsList = itemsList;
         this.promotionItems = promotionItems;
+        itemsMap = itemsList.getItemsMap();
     }
 
     
     public int calculatePrice() {
-        Map<Item, Integer> itemsMap = itemsList.getItemsMap();
-        int promotionValue = 0;
         int totalPromotionValue = 0;
         int totalPrices = 0;
 
@@ -29,26 +27,34 @@ public class TotalPrice {
         for(Map.Entry<Item, Integer> entry: itemsMap.entrySet()){
             int itemQuantity = entry.getValue();
             int itemPrice = entry.getKey().getPrice();
-
             totalPrices += itemQuantity * itemPrice;
         }
 
+        totalPromotionValue = calculatePromotionMultiPrice() + calculatePromotionOneFree() + calculatePromotionMealDeal();
+
+        return totalPrices - totalPromotionValue;
+    }
+
+    public int calculatePromotionMultiPrice(){
+        int promotionValue = 0;
         Item targetMultiPriceItem = promotionItems.getPromotionMultiPrice().getMultiPriceItem();
         if(itemsMap.containsKey(targetMultiPriceItem)){
             promotionValue = promotionItems.getPromotionMultiPrice().calculatePromotion(itemsMap.get(targetMultiPriceItem));
-            totalPromotionValue += promotionValue;
         }
+        return promotionValue;
+    }
 
-
-
+    public int calculatePromotionOneFree(){
+        int promotionValue = 0;
         Item targetOneFreeItem = promotionItems.getPromotionOneFree().getOneFreeItem();
         if(itemsMap.containsKey(targetOneFreeItem)){
             promotionValue = promotionItems.getPromotionOneFree().calculatePromotion(itemsMap.get(targetOneFreeItem));
-            totalPromotionValue += promotionValue;
         }
+        return promotionValue;
+    }
 
-
-
+    public int calculatePromotionMealDeal(){
+        int promotionValue = 0;
         List<Item> targetMealDealItem = promotionItems.getPromotionMealDeal().getMealDealList();
         List<Integer> quantityList = new LinkedList<>();
         for(Item item : targetMealDealItem){
@@ -57,12 +63,9 @@ public class TotalPrice {
                 quantityList.add(quantity);
             }
         }
-        
         if(!quantityList.isEmpty() && quantityList.size()==targetMealDealItem.size()){
             promotionValue = promotionItems.getPromotionMealDeal().calculatePromotion(quantityList);
-            totalPromotionValue += promotionValue;
         }
-
-        return totalPrices - totalPromotionValue;
+        return promotionValue;
     }
 }
